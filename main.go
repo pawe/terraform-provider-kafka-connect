f4ebbfdef943872ea3634ba17449bb10e5dbe872
Tobias de Vries <tobias.de.vries.ext@riag-digital.com>
2022-07-25 09:45:36 +0200

    Plugin is now debuggable

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"flag"
+
 	c "github.com/Mongey/terraform-provider-kafka-connect/connect"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{ProviderFunc: c.Provider})
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "Set to true to run the provider with support for debuggers.")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
+		Debug:        debug,
+		ProviderAddr: "Mongey/kafka-connect",
+		ProviderFunc: c.Provider,
+	}
+
+	plugin.Serve(opts)
 }
