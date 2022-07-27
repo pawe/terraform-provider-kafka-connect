1736a0285a973e338d3834a8ad3fc69544f34d82
Tobias de Vries <tobias.de.vries.ext@riag-digital.com>
2022-07-27 09:16:51 +0200

    WIP: Support for adding a root certificate

--- connect/provider.go
+++ connect/provider.go
@@ -27,20 +27,25 @@ func Provider() *schema.Provider {
 				Optional:    true,
 				DefaultFunc: schema.EnvDefaultFunc("KAFKA_CONNECT_BASIC_AUTH_PASSWORD", ""),
 			},
-			"ssl_client_certificate": {
+			"ssl_client_certificate_path": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("KAFKA_CONNECT_SSL_CLIENT_CERTIFICATE", ""),
+				DefaultFunc: schema.EnvDefaultFunc("KAFKA_CONNECT_SSL_CLIENT_CERTIFICATE_PATH", ""),
 			},
-			"ssl_client_key": {
+			"ssl_client_key_path": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("KAFKA_CONNECT_SSL_CLIENT_KEY", ""),
+				DefaultFunc: schema.EnvDefaultFunc("KAFKA_CONNECT_SSL_CLIENT_KEY_PATH", ""),
 			},
-			"verify_server": {
+			"ssl_root_ca_path": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("KAFKA_CONNECT_SSL_ROOT_CA_PATH", ""),
+			},
+			"ssl_verify_server": {
 				Type:        schema.TypeBool,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("KAFKA_CONNECT_VERIFY_SERVER", true),
+				DefaultFunc: schema.EnvDefaultFunc("KAFKA_CONNECT_SSL_VERIFY_SERVER", true),
 			},
 		},
 		ConfigureFunc: providerConfigure,
@@ -64,24 +69,34 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 		client.SetBasicAuth(user, pass)
 	}
 
-	verifyServer := d.Get("verify_server").(bool)
-	if !verifyServer {
+	error := configureSSL(d, client)
+	return client, error
+}
+
+func configureSSL(resources *schema.ResourceData, client kc.HighLevelClient) error {
+	sslVerifyServer := resources.Get("ssl_verify_server").(bool)
+	if !sslVerifyServer {
+		// Must be done before any other ssl settings as this overrides the entire TLS Config
 		client.SetInsecureSSL()
 	}
 
-	// client cert
-	certificate := d.Get("ssl_client_certificate").(string)
-	certificateKey := d.Get("ssl_client_key").(string)
+	sslRootCaPath := resources.Get("ssl_root_ca_path").(string)
+	if sslRootCaPath != "" {
+		client.AddRootCertificate(sslRootCaPath)
+	}
 
-	if certificate != "" && certificateKey != "" {
-		certificate, error := tls.LoadX509KeyPair(certificate, certificateKey)
+	clientCertificatePath := resources.Get("ssl_client_certificate_path").(string)
+	clientCertificateKeyPath := resources.Get("ssl_client_key_path").(string)
+
+	var error error = nil
+	if clientCertificatePath != "" && clientCertificateKeyPath != "" {
+		var certificate tls.Certificate
+		certificate, error = tls.LoadX509KeyPair(clientCertificatePath, clientCertificateKeyPath)
 		if error != nil {
 			log.Printf("[ERROR] Loading certificate failed: %s", error)
-			return nil, error
 		} else {
 			client.SetClientCertificates(certificate)
 		}
-		return client, error
 	}
-	return client, nil
+	return error
 }
--- go.mod
+++ go.mod
@@ -9,3 +9,5 @@ require (
 )
 
 replace git.apache.org/thrift.git => github.com/apache/thrift v0.0.0-20180902110319-2566ecd5d999
+
+//replace github.com/ricardo-ch/go-kafka-connect => /Users/tobias/VscProjects/go-kafka-connect
